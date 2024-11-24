14b194ef5928986d26d177a2eecba43463c77323
AbdulH0423 <zain.ebrahim8@gmail.com>
2024-11-24 16:31:53 +0200

    initializing the firebase app
    initializing the firebase app

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ Make it so that when you click the 'Add to cart' button, whatever is written in 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+
+//Firebase App Initialization:
+const appSettings = {
+    databaseURL: "https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/"
+}
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
