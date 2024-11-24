ef9f09d023dba313dd7a87a2bf7fa87b76f2cde0
AbdulH0423 <zain.ebrahim8@gmail.com>
2024-11-24 16:31:08 +0200

    Importing Firebase Modules to js
    Importing Firebase Modules to js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
+//Firebase Setup
+//Importing Firebase Modules:
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
