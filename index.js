87ccb1ce4b8d0b9e55709f05ffb54c3aefec9c67
AbdulH0423 <zain.ebrahim8@gmail.com>
2024-11-24 16:38:29 +0200

    real time updates to the html
    real time updates to the html once an action happens

--- index.js
+++ index.js
@@ -28,8 +28,29 @@ const shoppingListEl = document.getElementById("shopping-list")
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    console.log(inputValue)
-})function appendItemToShoppingListEl(item) {
+    push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
+})
+
+onValue(shoppingListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearShoppingListEl()
+        
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToShoppingListEl(currentItem)
+        }    
+    } else {
+        shoppingListEl.innerHTML = "No items here... yet"
+    }
+})
+
 function clearShoppingListEl() {
     shoppingListEl.innerHTML = ""
 }
